5614e05b7eaacf16428c7e6df088452b45084351
phat16 <87140697+phat16@users.noreply.github.com>
2021-07-09 07:53:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Ỏ',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'anh xin lỗi nhaaa :<< tha anh nhaaa anh sai òi 😙',
     desc: 'EM YÊU ANH HONG :33 <3 ',
-    btnYes: 'YÊUUUUU <3 :33',
-    btnNo: 'ĐÉOOOO',
+    btnYes: 'YÊUUUUU <3 THA ANHH  :33',
+    btnNo: 'ĐÉOOOO YÊUUU ĐÉO THAAAA',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'NGOAN ANH THƯƠNG NHAA :33.',
+    messDesc: 'NGOAN ANH THƯƠNG MUA ĐỒ CHO NHAAA NHAA :33.',
     btnAccept: 'Okiiiii <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
